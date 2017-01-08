92d2b3638e7ff573327a50beb4b948075909bb2d
Kamil <kpiotrowski@comea.pl>
2017-01-08 04:08:00 +0100

    Formatowanie datetime

--- src/common/CommonFunc.java
+++ src/common/CommonFunc.java
@@ -6,9 +6,7 @@ import com.sun.org.apache.xerces.internal.impl.dv.xs.FloatDV;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -26,7 +24,7 @@ public abstract class CommonFunc {
             MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             messageDigest.update(pass.getBytes());
             byte[] hash = messageDigest.digest(pass.getBytes(StandardCharsets.UTF_8));
-            return new String(HexBin.encode(hash));
+            return HexBin.encode(hash);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             return "";
@@ -35,27 +33,29 @@ public abstract class CommonFunc {
 
     public static boolean comparePass(String pass, String hash) {
         pass = hashPass(pass);
-        if(pass.equals(hash)) return true;
-        return false;
+        return pass.equals(hash);
     }
 
     public static java.sql.Date parseDateToSQL(String date) throws ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
         Date parsed = format.parse(date);
-        java.sql.Date sql = new java.sql.Date(parsed.getTime());
-        return sql;
+        return new java.sql.Date(parsed.getTime());
     }
     public static java.sql.Timestamp parseDateTimeToSQL(String datetime) throws ParseException {
         SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
         Date parsed = format.parse(datetime);
-        java.sql.Timestamp sql = new java.sql.Timestamp(parsed.getTime());
-        return sql;
+        return new java.sql.Timestamp(parsed.getTime());
     }
 
     public static String sqlDateToString(java.sql.Date date){
         DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
         if(date == null) return "";
         return df.format(date);
+    }
+    public static String sqlTimestampToString(Timestamp timestamp){
+        SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+        if(timestamp==null) return "";
+        return format.format(timestamp);
     }
 
     public static String emptyNullStr(String str){
@@ -65,24 +65,21 @@ public abstract class CommonFunc {
 
     public static Long strToLong(String str){
         try{
-            Long l = Long.parseLong(str);
-            return l;
+            return Long.parseLong(str);
         } catch (NumberFormatException e) {
             return null;
         }
     }
     public static Integer strToInteger(String str){
         try{
-            Integer l = Integer.parseInt(str);
-            return l;
+            return Integer.parseInt(str);
         } catch (NumberFormatException e) {
             return null;
         }
     }
     public static Float strToFloat(String str){
         try {
-            Float f = Float.parseFloat(str);
-            return f;
+            return Float.parseFloat(str);
         } catch (NumberFormatException e) {
             return null;
         }
@@ -103,6 +100,10 @@ public abstract class CommonFunc {
     public static void statSetVarPar(PreparedStatement pstmt, int index, java.sql.Date val) throws SQLException {
         if(val==null) pstmt.setNull(index, Types.DATE);
         else pstmt.setDate(index, val);
+    }
+    public static void statSetVarPar(PreparedStatement pstmt, int index, Timestamp val) throws SQLException {
+        if(val==null) pstmt.setNull(index, Types.FLOAT);
+        else pstmt.setTimestamp(index, val);
     }
 
 
