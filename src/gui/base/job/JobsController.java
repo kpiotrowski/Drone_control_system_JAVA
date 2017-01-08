e7259bcdaa588087edc997fe17b7f8fcad58381c
Kamil <kpiotrowski@comea.pl>
2017-01-08 04:08:29 +0100

    GUI dodawania i zmiany zadania

--- src/gui/base/job/JobsController.java
+++ src/gui/base/job/JobsController.java
@@ -17,8 +17,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static common.CommonFunc.parseDateTimeToSQL;
-import static common.CommonFunc.strToInteger;
+import static common.CommonFunc.*;
+import static common.CommonTask.onSuccessSimpleError;
 import static dataModels.Zadanie.statusStr;
 import static dataModels.Zadanie.typesStr;
 
@@ -50,12 +50,35 @@ public class JobsController {
     public ChoiceBox<Pair> jobFindStatus;
     public TabPane tabPane;
     public Tab findTab;
+    public Tab infoTab;
+    
+    public TextField jobInfoId;
+    public TextField jobInfoStartDate;
+
+    public Button jobInfoUpdate;
+    public Button jobInfoDelete;
+    public Button jobInfoDownloadResults;
+    public Tab createTab;
+    public Label jobCreateError;
+    public Button jobCreateButton;
+    public ChoiceBox<Pair<Integer, String>> jobCreateType;
+    public TextField jobCreateStartDate;
+    public ChoiceBox<Trasa> jobCreateRoute;
+    public TextField jobCreateDrone;
+    public ChoiceBox<Punkt_kontrolny> jobCreateFinishPoint;
+    public Label jobInfoError;
+
+    private Zadanie selectedJob;
 
 
     public JobsController(){}
 
     @FXML
     private void initialize() {
+        this.jobCreateButton.setOnAction(event -> this.create());
+        this.jobInfoUpdate.setOnAction(event -> this.update());
+        this.jobInfoDelete.setOnAction(event -> this.delete());
+        this.jobInfoDownloadResults.setOnAction(event -> this.downloadResults());
         this.findTab.setOnSelectionChanged(event -> {
             if(this.findTab.isSelected())this.reloadData();});
         this.tableView.setRowFactory( tv -> {
@@ -72,7 +95,7 @@ public class JobsController {
         this.columnDataStart.setCellValueFactory(new PropertyValueFactory<>("data_rozpoczenia"));
         this.columnType.setCellValueFactory(new PropertyValueFactory<>("typeString"));
         this.columnRouteName.setCellValueFactory(new PropertyValueFactory<>("trasa_nazwa"));
-        this.columnUserId.setCellValueFactory(new PropertyValueFactory<>("trasa_uzytkownik_id"));
+        this.columnUserId.setCellValueFactory(new PropertyValueFactory<>("uzytkownik_id"));
         this.columnDrone.setCellValueFactory(new PropertyValueFactory<>("dron_id"));
         this.columnFinishPoint.setCellValueFactory(new PropertyValueFactory<>("punkt_koncowy_id"));
         this.columnStatus.setCellValueFactory(new PropertyValueFactory<>("stanString"));
@@ -101,16 +124,140 @@ public class JobsController {
             data2.add(p);
         }
         jobFindStatus.setItems(data2);
+        this.jobCreateType.setOnAction(event -> {
+            if(this.jobCreateType.getValue()!=null) {
+                if (this.jobCreateType.getValue().getKey() == Zadanie.TYPE_MOVE_TO_POINT) {
+                    this.jobCreateDrone.setDisable(false);
+                    this.jobCreateFinishPoint.setDisable(false);
+                    this.jobCreateRoute.setDisable(true);
+                    this.jobCreateRoute.setValue(null);
+                } else {
+                    this.jobCreateDrone.setDisable(true);
+                    this.jobCreateFinishPoint.setDisable(true);
+                    this.jobCreateDrone.setText("");
+                    this.jobCreateFinishPoint.setValue(null);
+                    this.jobCreateRoute.setDisable(false);
+                }
+            }
+        });
     }
 
+    private Zadanie parseCreateForm(){
+        Zadanie z = new Zadanie();
+        if(jobCreateType.getValue()!=null) z.setTyp(jobCreateType.getValue().getKey());
+        try {
+            z.setData_rozpoczenia(parseDateTimeToSQL(jobCreateStartDate.getText()));
+        } catch (ParseException e) {
+            this.jobCreateError.setText("Incorrect datetime format. Correct: dd-MM-yyyy HH:mm");
+            return null;
+        }
+        if(jobCreateRoute.getValue()!=null){
+            z.setTrasa_uzytkownik_id(jobCreateRoute.getValue().getUzytkownik_id());
+            z.setTrasa_nazwa(jobCreateRoute.getValue().getNazwa());
+        }
+        z.setDron_id(strToInteger(jobCreateDrone.getText()));
+        z.setStan(Zadanie.STATUS_NOWE_ZADANIE);
+        z.setUzytkownik_id(Main.authenticatedUser.getId());
+        if(jobCreateFinishPoint.getValue()!=null) z.setPunkt_koncowy_id(jobCreateFinishPoint.getValue().getId());
+        return z;
+    }
+    private void create(){
+        Zadanie z = parseCreateForm();
+        if(z==null) return;
+        Error err = Main.zadanieService.validate(z);
+        if(err != null) {
+            this.jobCreateError.setText(err.getMessage());
+            return;
+        }
+        Task t = new Task() {
+            @Override
+            protected Object call() throws Exception {
+                return Main.zadanieService.insert(z);
+            }
+        };
+        t.setOnSucceeded(event -> {
+            Error e = (Error) t.getValue();
+            if(e != null)
+                Main.gui.showDialog("Błąd", "Niepowodzenie podczas dodawania zadania", e.getMessage(), Alert.AlertType.ERROR);
+            else {
+                Main.gui.showDialog("Info", "Pomyślnie dodano nowe zadanie", "", Alert.AlertType.INFORMATION);
+                this.clearCreateForm();
+            }
+        });
+        new Thread(t).start();
+    }
+    private void update(){
+        this.jobInfoError.setText("");
+        Zadanie zz = new Zadanie();
+        zz.setId(this.selectedJob.getId());
+        String datetime = this.jobInfoStartDate.getText();
+        if(datetime==null){
+            this.jobFindError.setText("Start date cannot be empty");
+            return;
+        }
+        try {
+            zz.setData_rozpoczenia(parseDateTimeToSQL(datetime));
+            Task t = new Task() {
+                protected Object call() {
+                    return Main.zadanieService.update(zz);
+                }
+            };
+            t.setOnSucceeded(
+                    onSuccessSimpleError(t,"Pomyślnie zaktualizowano zadanie o dronie","Niepowodzenie podzczas aktualizacji zadania")
+            );
+            new Thread(t).start();
+        } catch (ParseException e) {
+            this.jobInfoError.setText("Incorrect datetime format");
+        }
+
+    }
+    private void delete(){
+        Integer id = this.selectedJob.getId();
+        Integer droneId = this.selectedJob.getDron_id();
+        Task t = new Task() {
+            protected Error call() { return Main.zadanieService.delete(id,droneId); }
+        };
+        t.setOnSucceeded(event -> {
+                Error e = (Error) t.getValue();
+                if(e != null)
+                    Main.gui.showDialog("Błąd", "Niepowodzenie usuwania zadania", e.getMessage(), Alert.AlertType.ERROR);
+                else {
+                    Main.gui.showDialog("Info", "Pomyślnie usunięto zadanie", "", Alert.AlertType.INFORMATION);
+                    this.clearInfoForm();
+                }
+        });
+        new Thread(t).start();
+    }
+    private void downloadResults(){
+        Main.gui.showDialog("RESULT","Now you should download your results.","", Alert.AlertType.INFORMATION);
+        //TODO IMEPLEMENT THIS
+    }
+    private void clearCreateForm(){
+        this.jobCreateError.setText("");
+        jobCreateType.setValue(null);
+        jobCreateStartDate.setText("");
+        jobCreateRoute.setValue(null);
+        jobCreateDrone.setText("");
+        jobCreateFinishPoint.setValue(null);
+    }
     public void refreshPermissions(Uzytkownik uz){
+        Integer maxjobType=Zadanie.TYPE_MOVE_TO_POINT;
         if (uz.getPoziom_uprawnien() == 0){
             this.jobFindUserLabel.setVisible(false);
             this.jobFindUser.setVisible(false);
+            maxjobType--;
         } else {
             this.jobFindUserLabel.setVisible(true);
             this.jobFindUser.setVisible(true);
         }
+        ObservableList<Pair<Integer,String>> data = FXCollections.observableArrayList();
+        for(int i=0;i<typesStr.length && i<=maxjobType;i++){
+            Pair<Integer,String> p = new Pair<Integer, String>(i,typesStr[i]){
+                public String toString() {return getValue();}
+            };
+            data.add(p);
+        }
+        jobCreateType.setItems(data);
         reloadData();
     }
 
@@ -119,6 +266,7 @@ public class JobsController {
         t.setOnSucceeded(event -> {
             List<Punkt_kontrolny> resultList = (List<Punkt_kontrolny>) t.getValue();
             setPointValues(jobFindFinishPoint, resultList);
+            setPointValues(jobCreateFinishPoint, resultList);
         });
         new Thread(t).start();
 
@@ -129,15 +277,30 @@ public class JobsController {
         tt.setOnSucceeded(event -> {
             List<Trasa> resultList2 = (List<Trasa>) tt.getValue();
             setRouteValues(jobFindRoute, resultList2);
+            setRouteValues(jobCreateRoute,resultList2);
         });
         new Thread(tt).start();
-
-
-        //TODO IMPLEMENT THIS
     }
 
     private void setInfoForm(Zadanie z){
-        //TODO IMPLEMENT THIS
+        this.jobInfoId.setText(String.valueOf(z.getId()));
+        this.jobInfoStartDate.setText(sqlTimestampToString(z.getData_rozpoczenia()));
+        if(z.getStan()<= Zadanie.STATUS_NOWE_ZADANIE) this.jobInfoUpdate.setDisable(false);
+        else this.jobInfoUpdate.setDisable(true);
+        if(z.getStan()<= Zadanie.STATUS_PRZYDZIELONO_DRONA) this.jobInfoDelete.setDisable(false);
+        else this.jobInfoDelete.setDisable(true);
+        if(z.getStan()==Zadanie.STATUS_ZAKONCZONE) this.jobInfoDownloadResults.setDisable(false);
+        else this.jobInfoDownloadResults.setDisable(true);
+        this.selectedJob=z;
+        this.tabPane.getSelectionModel().select(this.infoTab);
+    }
+    private void clearInfoForm(){
+        this.jobInfoDownloadResults.setDisable(true);
+        this.jobInfoId.setText("");
+        this.jobInfoDelete.setDisable(true);
+        this.jobInfoUpdate.setDisable(true);
+        this.jobInfoStartDate.setText("");
+        this.selectedJob=null;
     }
 
     private void clearFindForm(){
